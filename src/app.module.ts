9ea74d76155528afc154f297887fc712dafcea8c
sf <samia.f@escolar.ifrn.edu.br>
2025-09-29 03:40:01 +0000

    continuando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'tasks.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Apenas para desenvolvimento
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: true, // Em produção, especifique domínios específicos
+    credentials: true,
+  });
+
+  // Habilitar validação global
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  await app.listen(3000);
+  console.log('API rodando em http://localhost:3000');
 }
 bootstrap();
