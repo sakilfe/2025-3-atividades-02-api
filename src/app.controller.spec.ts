4423ca60cd665503d0b7c711091ea35905523d10
ssf! <123976455+sakilfe@users.noreply.github.com>
2025-11-27 14:48:16 +0000

    pré-teste

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,16 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getInfo', () => {
+    it('deve retornar informações da API com status, version e description', () => {
+      const result = appController.getInfo();
+      
+      expect(result).toHaveProperty('status');
+      expect(result).toHaveProperty('version');
+      expect(result).toHaveProperty('description');
+      expect(result.status).toBe('online');
+      expect(result.version).toBe('1.0.0');
+      expect(result.description).toBe('Esta é API de tarefas (todos) da turma de Infoweb 2025.');
     });
   });
 });
